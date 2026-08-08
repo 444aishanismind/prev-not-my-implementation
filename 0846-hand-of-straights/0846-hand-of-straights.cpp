f34db47f16c1eb1a594deac4f9f82297fd643b48
Aishani <444aishani.d@gmail.com>
2026-08-08 08:54:53 +0530

    Time: 40 ms (47.91%), Space: 32.4 MB (42.44%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -4,18 +4,18 @@
 class Solution {
 public:
     bool isNStraightHand(std::vector<int>& hand, int groupSize) {
-       //If cards can't be evenly divided into groups, return false instantly
+      
         if (hand.size() % groupSize != 0) {
             return false;
         }
 
-        //  Count frequencies. std::map automatically sorts keys in ascending order
+        
         std::map<int, int> countMap;
         for (int card : hand) {
             countMap[card]++;
         }
 
-        // Iterate through cards from smallest to largest magnitude
+        
         for (auto& [card, count] : countMap) {
             if (count > 0) {
                 int freq = count; // The number of groups this card must start
@@ -30,6 +30,6 @@ public:
             }
         }
 
-        return true; // Added this to satisfy the non-void return requirement
+        return true;
     }
 };
